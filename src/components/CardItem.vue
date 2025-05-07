a2693076a8dad5d6532755a7368cd0f49909e477
Anton Vlasov <antonvlasov@MacBook-Pro-MacBook.local>
2025-05-07 18:37:15 +0500

    init

---
+++ src/components/CardItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div class="card-item">
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.card-item {
+  border: 1px solid #000;
+  padding: 16px;
+  border-radius: 8px;
+}
+</style>
